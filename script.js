ded4a2ce4b645e5a62a06c75e7bc98fd652bd5f6
Andrew Alagna <aalagna04@gmail.com>
2022-03-03 08:06:14 -0500

    remove row from grid

--- script.js
+++ script.js
@@ -4,20 +4,35 @@ let colorSelected;
 
 //Add a row
 function addR() {
-    let row = document.createElement("tr");
-    if(numCols == 0){
-        let data = document.createElement("td");
-        row.appendChild(data)
-        numRows += 1
+    let grid = document.getElementById('grid')
+    let rows = document.getElementsByTagName('tr')
+
+    if(rows.length == 0){
+        let row = document.createElement('tr')
+        let col = document.createElement("td");
+        col.onclick = function(){ this.style.background = colorSelected }
+        row.appendChild(col)
+        grid.appendChild(row)
+        numRows++
+    }
+    else if(rows.length >= 0 && numCols == 0){
+        let row = document.createElement('tr')
+        let col = document.createElement("td");
+        col.onclick = function(){ this.style.background = colorSelected }
+        row.appendChild(col)
+        grid.appendChild(row)
+        numRows++
     }
     else{
+        let size = document.getElementsByTagName('table').lastElementChild
+        let row = document.createElement('tr')
         for(let i=0; i < numCols; i++){
             let data = document.createElement("td");
             row.appendChild(data)
-            numRows++
         }
+        numRows++
+        grid.appendChild(row)
     }
-    document.querySelector("table").appendChild(row)
 }
 //Add a column
 function addC() {
@@ -28,8 +43,8 @@ function addC() {
 
 //Remove a row
 function removeR() {
-    let row = document.querySelector("tr");
-    let lastRow = row.lastElementChild;
+    let table = document.querySelector("table");
+    let lastRow = table.lastElementChild;
     document.querySelector("table").removeChild(lastRow);
 }
 //Remove a column
